26b694ce543c07ce400e0049fc07c4ceae6567b9
isuru <isurudevj@gmail.com>
2024-12-10 06:52:44 +0800

    Project set up wip.

---
+++ media-driver/src/main/java/io/github/busy_spin/media_driver/MediaDriver.java
@@ -0,0 +1,8 @@
+package io.github.busy_spin.media_driver;
+
+public class MediaDriver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world !!!");
+    }
+}
---
+++ media-driver/src/test/java/HelloTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    void testAssert() {
+        Assertions.assertEquals(2, 1+1);
+    }
+
+}
