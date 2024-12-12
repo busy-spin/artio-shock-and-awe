3bbd3c905dfa4fcb1f9716c9df443d6aa40bf912
isuru <isurudevj@gmail.com>
2024-12-13 05:26:47 +0800

    Project set up wip.

--- fix-engine/src/main/java/io/github/busy_spin/artio/fix_engine/FixEngineApp.java
+++ fix-engine/src/main/java/io/github/busy_spin/artio/fix_engine/FixEngineApp.java
@@ -2,6 +2,6 @@ package io.github.busy_spin.artio.fix_engine;
 
 public class FixEngineApp {
     public static void main(String[] args) {
-        System.out.println("Fix engine hello world !!!");
+
     }
 }
